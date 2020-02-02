0afa6947dc6f1ea32b5ca84c9d269078e7c39ed3
ldengra <hiker_luis@hotmail.com>
2020-02-03 00:27:16 +0100

    actualizado desde windows

--- src/main/java/com/luisdengra/practica3/Bombo.java
+++ src/main/java/com/luisdengra/practica3/Bombo.java
@@ -5,7 +5,9 @@ public class Bombo {
     /**
      * Atributos
      */
-    private int[] bomboPrincipal; //este bombo tiene que generar 6 numeros mas 1 complementario, array de 7 huecos
+    private int[] bomboPrincipal; //este bombo tiene que generar 6 numeros mas 1 complementario, array de 6 huecos
+    private final int MAX_PRINCIPAL = 6;
+    private int bomboComplementario;
     private int bomboReintegro; //generar un numero del 0 al 9.
 
     /**
@@ -14,6 +16,36 @@ public class Bombo {
 
     public Bombo(int[] bomboPrincipal, int bomboReintegro) {
         this.bomboPrincipal = bomboPrincipal;
-        this.bomboReintegro = bomboReintegro;
+        this.bomboComplementario = Lib.aleatorio(0,49);
+        this.bomboReintegro = Lib.aleatorio(0,9);
+    }
+
+    public int getBomboComplementario(){
+        return bomboComplementario;
+    }
+
+    public int getBomboReintegro() {
+        return bomboReintegro;
+    }
+
+    @Override
+    public String toString() {
+        String aux = "";
+        aux += String.format("Los numeros premiados son: %24s \n", getBomboString());
+        aux += String.format("Y el reintegro es: %3s", bomboReintegro);
+        return aux;
+    }
+
+    public int[] getBomboPrincipal(){
+        return bomboPrincipal;
+    }
+
+    public String getBomboString(){
+        String aux = "";
+        for(int i = 0; i < MAX_PRINCIPAL; i++){
+            aux += String.format("%4s", getBomboPrincipal()[i]);
+        }
+        return aux;
     }
 }
+
